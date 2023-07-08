a5351c39b736941f8ddf9dc4ee39a505e1c233da
Alejandro Chavez <achavezr5@wisc.edu>
2023-07-07 20:34:21 -0500

    Improve amortization table appearance

--- Scripts/main.js
+++ Scripts/main.js
@@ -2,27 +2,32 @@ const NUM_MONTHS_IN_YEAR = 12;
 const SPACE_CHAR = ' '; 
 const LANG = "en";
 const DEBUG = false;
+const AMORTIZATION_TABLE_HEADERS = ["MONTH", "STARTING AMOUNT", "ENDING AMOUNT", "INTEREST PAID", "PRINCIPAL PAID", "TOTAL PAID"];
 
 // Calculates and returns an array of elements representing months until the loan is paid 
 function createAmortizationTable(startingAmount, payment, interestRate) {
     let amortizationArr = [];
+    let totalPaid = 0;
 
     amortizationArr.push({
         interestAmount: 0,
         principalPayment: 0,
         monthStartingAmount: startingAmount, 
-        monthEndAmount: startingAmount
+        monthEndAmount: startingAmount,
+        totalPaid: totalPaid
     })
 
     while (startingAmount > 0) {
         let interestAmount = calculateMonthlyInterest(startingAmount, interestRate);
         let principalPayment = payment - interestAmount;
         let afterPaymentAmount = startingAmount + interestAmount - payment;
+        totalPaid += startingAmount - afterPaymentAmount;
         amortizationArr.push({
             interestAmount: interestAmount,
             principalPayment: principalPayment,
             monthStartingAmount: startingAmount,
-            monthEndAmount: afterPaymentAmount > 0 ? afterPaymentAmount : 0
+            monthEndAmount: afterPaymentAmount > 0 ? afterPaymentAmount : 0,
+            totalPaid: totalPaid
         });
         startingAmount = afterPaymentAmount;
     }
@@ -60,7 +65,7 @@ function padValueWithSpaces(value, numOfSpaces) {
 // TESTS
 
 // Creation of amortization array 
-function test1() { 
+function generateTextRepresentationOfTable() { 
     let amortArr = [
         {
             interestAmount: "INTEREST PAID", 
@@ -78,7 +83,7 @@ function test1() {
         let month = amortArr[i];
         let newElem = document.createElement("p");
         let fields = [
-            padValueWithSpaces(i - 2 > 0 ? i : " ", 15),
+            padValueWithSpaces(i, 15),
             padValueWithSpaces(month.monthStartingAmount,15),
             padValueWithSpaces(month.monthEndAmount,15),
             padValueWithSpaces(month.interestAmount, 15),
@@ -94,11 +99,49 @@ function test1() {
 
     }
 }
-function test2 () {
-    
+
+function generateHTMLRepresentationOfTable() {
+    let amortArr = createAmortizationTable(50000, 600, 4);
+    let amortTableDiv = document.getElementById("amortizationTable");
+    let amortTable = document.createElement("table");
+
+    function getAmortizationTableHeader() {
+        let headerRow = document.createElement("tr");
+        for (let i in AMORTIZATION_TABLE_HEADERS) {
+            let header = document.createElement("th");
+            header.innerText = AMORTIZATION_TABLE_HEADERS[i];
+            headerRow.appendChild(header);
+        }
+        return headerRow;
+    }
+
+    amortTable.appendChild(getAmortizationTableHeader());
+
+    // generate amortization table representation in HTML
+    for (let i in amortArr) { 
+        let month = amortArr[i];
+        let newRow = document.createElement("tr");
+        let fields = [
+            padValueWithSpaces(i, 15),
+            padValueWithSpaces(month.monthStartingAmount,15),
+            padValueWithSpaces(month.monthEndAmount,15),
+            padValueWithSpaces(month.interestAmount, 15),
+            padValueWithSpaces(month.principalPayment, 15),
+            padValueWithSpaces(month.totalPaid, 15)
+        ];
+
+        for (let i in fields) {
+            let newTableData = document.createElement("td");
+            newTableData.innerText = fields[i];
+            newRow.appendChild(newTableData);
+        }
+
+        amortTable.append(newRow);
+    }
+    amortTableDiv.appendChild(amortTable);
 }
 
 
 // ##### RUN TESTS HERE #####
-// test1();
-test2(); 
+// generateTextRepresentationOfTable();
+generateHTMLRepresentationOfTable(); 
